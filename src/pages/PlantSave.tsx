c3da23f3f148409db9500124c5f3657769cb09d0
alxac <alxac@msn.com>
2021-04-22 21:39:08 -0400

    Começoa da Tela PlantSave

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,14 +1,108 @@
-import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import { useRoute } from "@react-navigation/core";
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 import { SvgFromUri } from "react-native-svg";
+import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
+import waterdropImg from "../../src/assets/waterdrop.png";
+import { Button } from "../components/Button";
 
-export function PlantSave(){
-    return (
-        <View>
-            <SvgFromUri uri="" height={150} width={150} />
-            <Text >
-sssssssssssssssssssssss
-            </Text>
-        </View>
-    )
+interface Params {
+  plant: {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+      times: number;
+      repeat_every: string;
+    };
+  };
 }
+
+export function PlantSave() {
+  const route = useRoute();
+  const { plant } = route.params as Params;
+
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.plantInfo}>
+          <SvgFromUri uri={plant.photo} height={150} width={150} />
+          <Text style={styles.plantName}>{plant.name}</Text>
+          <Text style={styles.plantAbout}>{plant.about}</Text>
+        </View>
+      </View>
+      <View style={styles.controller}>
+        <View style={styles.tipConteiner}>
+          <Image source={waterdropImg} style={styles.tipImage} />
+          <Text style={styles.tipText}>Lorem</Text>
+        </View>
+        <Text style={styles.alertLabel}>
+            Escolha o melhor horário para ser lembrado:
+        </Text>
+        <Button title="Cadastrar Planta" onPress={() => {}} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    backgroundColor: colors.shape,
+  },
+  plantInfo: {
+    flex: 1,
+    paddingHorizontal: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.shape,
+  },
+  controller: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20,
+  },
+  plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15,
+  },
+  tipConteiner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: colors.blue_light,
+    paddingBottom: 20,
+    borderRadius: 20,
+    position: "relative",
+    bottom: 60,
+  },
+  tipImage: {
+    width: 56,
+    height: 56,
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 17,
+    textAlign: "justify",
+  },
+  alertLabel: {
+    textAlign: "center",
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+    marginBottom: 5,
+  },
+  plantAbout: {},
+});
